924b5e3a24cb6099eb51e857849ebe0308a3a3cd
GE77 <56392482+skyriyvrzo@users.noreply.github.com>
2024-02-29 21:31:45 +0700

    Update Main.java

--- src/main/java/com/get/discord/gg/Main.java
+++ src/main/java/com/get/discord/gg/Main.java
@@ -1,31 +1,8 @@
 package com.get.discord.gg;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import com.get.discord.gg.gui.LogGUI;
-import com.get.discord.gg.util.CheckOS;
-import com.get.discord.gg.util.CheckVersion;
-import com.get.discord.gg.util.Reference;
-import com.get.discord.gg.util.UnpackAssets;
-import com.get.lib.github.Github;
-import com.get.lib.logutils.LogUtil;
-
 public final class Main {
-
-	public final static Properties properties = new Properties(); 
-	public static LogUtil log;
-	public static Github github = new Github("skyriyvrzo", "Discord", Reference.VERSIONS);
 	
 	public static void main(String[] a) throws IOException {
-		CheckOS.checkOperatingSystem();
-		
-		log = new LogUtil(Reference.getDirectory.get());
-			
-		CheckVersion.changeVersionFile();
-		UnpackAssets.extractFile();
-		
-		log.mkdir();
-		new LogGUI();
+		System.out.println("Hello World!");
 	}
 }
