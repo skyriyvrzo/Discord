dfbdb46b261a18aa85f8522b9a75a066789b63af
GE77 <56392482+skyriyvrzo@users.noreply.github.com>
2024-02-29 21:32:04 +0700

    Update Main.java

--- src/main/java/com/get/discord/gg/Main.java
+++ src/main/java/com/get/discord/gg/Main.java
@@ -2,7 +2,7 @@ package com.get.discord.gg;
 
 public final class Main {
 	
-	public static void main(String[] a) throws IOException {
+	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
 }
