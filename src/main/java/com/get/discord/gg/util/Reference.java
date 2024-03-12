f204dbc1e86f1d74f83ef64bd6478d3ee6be35f6
GE77 <skyriyvrzo@gmail.com>
2024-03-12 11:04:10 +0700

    1.4.5-RC6

--- src/main/java/com/get/discord/gg/util/Reference.java
+++ src/main/java/com/get/discord/gg/util/Reference.java
@@ -6,7 +6,7 @@ import com.get.discord.gg.Main;
 
 public final class Reference {
 
-	public static final String VERSIONS = "1.4.5-RC5";
+	public static final String VERSIONS = "1.4.5-RC6";
 	
 	public static Supplier<String> getDirectory = () -> CheckOS.directory;
 	public static Supplier<String> getClassPath = () -> Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
