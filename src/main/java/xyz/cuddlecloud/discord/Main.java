b9ca3b7a4e01c8f060b81488dc9d6c4285f05f7c
GE77 <skyriyvrzo@gmail.com>
2024-06-28 22:20:24 +0700

    main

--- src/main/java/xyz/cuddlecloud/discord/Main.java
+++ src/main/java/xyz/cuddlecloud/discord/Main.java
@@ -1,7 +1,7 @@
-package com.get.discord.gg;
+package xyz.cuddlecloud.discord;
 
 public final class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Discord!");
 	}
 }
